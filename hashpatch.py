6cdceb2cf7aadd9e0dce3663d0d21871de6eae3f
Ryan Helinski <rlhelinski@gmail.com>
2012-05-26 08:56:15 -0600

    New version

--- hashpatch.py
+++ hashpatch.py
@@ -8,10 +8,12 @@ sha512ext = ".sha512"
 numSkippedFiles = 0
 
 class hashMap:
+	# Variables declared here are class-static
 
 	def __init__(self, rootPath="", source=None):
 		self.rootPath = ""
 		self.hashDict = dict()
+		self.reverseDict = dict()
 		self.savePath = ""
 
 		if (rootPath != "" and os.path.isdir(rootPath)):
@@ -24,17 +26,22 @@ class hashMap:
 
 	def __len__(self):
 		# this is the number of unique hashes. With a reverse map, I could easily list the number of files
-		return (len(self.hashDict))
+		#return (len(self.hashDict))
+		return (len(self.reverseDict))
 
 	# This function finds the hash of a file based on its path
-	def findPath (self, searchPath):
-		# This is not a very efficient solution---a reverse map could be built
-		for key, pathList in self.hashDict.items():
-			for path in pathList:
-				if (searchPath == path):
-					return key
+	def findByPath (self, searchPath):
+		if (searchPath not in self.reverseDict):
+			return False
+		else:
+			return self.reverseDict[searchPath]
 
-		return False
+		# This is not a very efficient solution---a reverse map could be built
+		#for key, pathList in self.hashDict.items():
+			#for path in pathList:
+				#if (searchPath == path):
+					#return key
+		#return False
 
 	# This function builds a hash dict object for a files in a path
 	def update(self, pattern=".*", verbose=False):
@@ -53,7 +60,7 @@ class hashMap:
 					# If we are expanding and this path is anywhere in the dictionary,
 					# do not update the hash
 					#if (expand and (mypath in list(itertools.chain(hashDict.values())))):
-					if (expanding and (self.findPath(mypath) != False)):
+					if (expanding and (self.findByPath(mypath) != False)):
 						numSkippedFiles += 1
 						if (verbose and ((numSkippedFiles % 100) == 0)):
 							print ".",
@@ -73,6 +80,8 @@ class hashMap:
 							# Duplicate file found
 							# This hash is already associated with a list
 							self.hashDict[myhash.digest()].append( mypath )
+						# also map the path to the hash
+						self.reverseDict[mypath] = myhash.digest()
 							
 						if (verbose):
 							print myhash.hexdigest() + shaFieldSep + repr(self.hashDict[myhash.digest()]) 
@@ -80,7 +89,7 @@ class hashMap:
 					except IOError as error:
 						print error
 	
-		print "Ended with %d unique hashes" % len(self.hashDict)
+		print "Ended with %d files, %d unique hashes" % (len(self.reverseDict), len(self.hashDict))
 	
 		return self
 
@@ -102,6 +111,8 @@ class hashMap:
 			else: 
 				# This hash is already associated with a list
 				self.hashDict[myhash].append( mypath )
+			# also map the path to the hash
+			self.reverseDict[mypath] = myhash
 								
 			#print lineHash + shaFieldSep + repr(hashDict[myhash])
 
@@ -190,28 +201,45 @@ def deleteInDest (sourceMap, destMap, act=False):
 
 	rf = open('hashpatch.report', 'a')
 
-	for key, val in destDict.items():
-		if (key in sourceDict):
+	for key, val in destMap.hashDict.items():
+		if (key in sourceMap.hashDict):
 			foundDup += 1
 		
 			if act:
 				for path in val:
 					print "Removing '%s'" % path
-					#os.remove(path)
+					os.remove(path)
 					# delete the parent directory if it is empty
-					# This should be recursive!
-					parentDir = os.path.dirname(path)
-					if (not os.listdir(parentDir)):
-						print "Removing empty directory '%s'" % parentDir
-				
-					return False
-
-				#if (not os.path.exists(destSubDir)):
-				#	os.makedirs(destSubDir)
-				
-				#shutil.copy2(val[0], destSubDir)
-
+					if (not os.listdir(os.path.dirname(path))):
+						# This is a function that recursively removes empty directories
+						os.removedirs(os.path.dirname(path))
 
 	print "%s %d duplicate files in destination" % ("Deleted" if act else "Found", foundDup)
 	rf.close()
 	
+def deleteBrokenLinks (path, act=False):
+	
+	for root, dirs, files in os.walk(path):
+		for filename in files:
+			#print filename
+			if (os.path.islink(os.path.join(root, filename))):
+				#print "Link at %s/%s" % (root, filename)
+				if(not os.path.exists(os.path.join(root, filename))):
+					if (act):
+						os.remove(os.path.join(root, filename))
+						if (not os.listdir(root)):
+							# This is a function that recursively removes empty directories
+							os.removedirs(root)
+					print "%s broken link at %s/%s" % ("Deleted" if act else "Found", root, filename)
+					
+def deleteEmptyDirs (path, act=False):		
+
+	for root, dirs, files in os.walk(path):
+		#for dir in dirs:
+		if (not os.listdir(root)):
+			# This is a function that recursively removes empty directories
+			if (act):
+				os.removedirs(root)
+			print "%s empty directory at %s" % ("Deleted" if act else "Found", root)
+
+
