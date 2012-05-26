9e2233f5a12f1023d615538cf8a5ec5d87a5a278
Ryan Helinski <rlhelinski@gmail.com>
2012-05-26 08:58:20 -0600

    New version

---
+++ hashtest.py
@@ -0,0 +1,4 @@
+import hashpatch 
+MainMap = hashpatch.hashMap("Compress", "Compress-hashpatch.sha512sum")
+DupeMap = hashpatch.hashMap("Compress-1260381162", "Compress-1260381162-hashpatch.sha512sum")
+#hashpatch.deleteInDest(MainMap, DupeMap, act=False)
